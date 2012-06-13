e57b9bd14ea1abbb592300bd22d963297d731144
Andy Nu <nuandy@gmail.com>
2012-06-13 18:47:15 -0400

    Add python script that creates a skeletal web app.

---
+++ bin/othello-create.py
@@ -0,0 +1,252 @@
+import os
+import sys
+import getopt
+
+xml_version = '1.0'
+encoding = 'UTF-8'
+app_key = ''
+
+options, extraparams = getopt.getopt(sys.argv[1:], '', ['app='])
+
+path = os.path.realpath('.')
+base = os.path.basename(path)
+
+def createapp():
+  if base == 'othello':
+    for opt, arg in options:
+      if opt in ('--app'):
+        app_key = arg
+        os.mkdir(os.path.abspath(os.curdir)+'/apps/'+app_key,0777)
+        os.chdir(os.path.abspath(os.curdir)+'/apps/'+app_key)
+        os.mkdir('WEB-INF',0777)
+        os.chdir('WEB-INF')
+        xmlstring = '<?xml version="' + xml_version + '" encoding="' + encoding  + '" ?>\n'
+        xmlstring += '<web-app xmlns="http://java.sun.com/xml/ns/javaee" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://java.sun.com/xml/ns/javaee http://java.sun.com/xml/ns/javaee/web-app_2_5.xsd" metadata-complete="true" version="2.5">\n'
+        xmlstring += '<display-name>' + app_key + ' app</display-name>\n'
+        xmlstring += '<session-config>\n'
+        xmlstring += '<session-timeout>20</session-timeout>\n'
+        xmlstring += '</session-config>\n'
+        xmlstring += '<servlet>\n'
+        xmlstring += '<servlet-name>default</servlet-name>\n'
+        xmlstring += '<servlet-class>org.mortbay.jetty.servlet.DefaultServlet</servlet-class>\n'
+        xmlstring += '<init-param>\n'
+        xmlstring += '<param-name>dirAllowed</param-name>\n'
+        xmlstring += '<param-value>false</param-value>\n'
+        xmlstring += '</init-param>\n'
+        xmlstring += '<init-param>\n'
+        xmlstring += '<param-name>welcomeServlets</param-name>\n'
+        xmlstring += '<param-value>true</param-value>\n'
+        xmlstring += '</init-param>\n'
+        xmlstring += '</servlet>\n'
+        xmlstring += '<welcome-file-list>\n'
+        xmlstring += '<welcome-file>public/index.html</welcome-file>\n'
+        xmlstring += '</welcome-file-list>\n'
+        xmlstring += '<servlet>\n'
+        xmlstring += '<servlet-name>BaseServlet</servlet-name>\n'
+        xmlstring += '<servlet-class>src.main.servlet.BaseServlet</servlet-class>\n'
+        xmlstring += '</servlet>\n'
+        xmlstring += '<servlet-mapping>\n'
+        xmlstring += '<servlet-name>default</servlet-name>\n'
+        xmlstring += '<url-pattern>/</url-pattern>\n'
+        xmlstring += '</servlet-mapping>\n'
+        xmlstring += '<context-param>\n'
+        xmlstring += '<description>Unique App Key</description>\n'
+        xmlstring += '<param-name>app.key</param-name>\n'
+        xmlstring += '<param-value>' + app_key + '</param-value>\n'
+        xmlstring += '</context-param>\n'
+        xmlstring += '<error-page>\n'
+        xmlstring += '<error-code>404</error-code>\n'
+        xmlstring += '<location>/app/views/http_status/404.html</location>\n'
+        xmlstring += '</error-page>\n'
+        xmlstring += '<error-page>\n'
+        xmlstring += '<error-code>500</error-code>\n'
+        xmlstring += '<location>/app/views/http_status/500.html</location>\n'
+        xmlstring += '</error-page>\n'
+        xmlstring += '</web-app>\n'
+        f = open('web.xml', 'w')
+        f.write(xmlstring)
+        f.close()
+        os.chdir(os.path.pardir)
+        os.mkdir('app',0777)
+        os.mkdir('conf',0777)
+        os.chdir('conf')
+        xmlstring = '<?xml version="' + xml_version + '" encoding="' + encoding  + '" ?>\n'
+        xmlstring += '<routes>\n'
+        xmlstring += '<!--<url controller="'+ app_key  +'.app.controllers.Index">/index.jsp</url>-->\n'
+        xmlstring += '</routes>\n'
+        f = open('routes.xml', 'w')
+        f.write(xmlstring)
+        f.close()
+        os.chdir(os.path.pardir)
+        os.mkdir('public',0777)
+        os.chdir('public')
+        htmlstring = '<html>\n'
+        htmlstring += '<head>\n'
+        htmlstring += '<title>'+ app_key +'</title>\n'
+        htmlstring += '</head>\n'
+        htmlstring += '<body>\n'
+        htmlstring += '<h1>Welcome to '+ app_key +'.</h1>\n'
+        htmlstring += '<h2>This app is powered by Othello!</h2>\n'
+        htmlstring += '</body>\n'
+        htmlstring += '</html>\n'
+        f = open('index.html', 'w')
+        f.write(htmlstring)
+        f.close()
+        os.chdir(os.path.pardir)
+        os.chdir('app')
+        os.mkdir('controllers',0777)
+        os.mkdir('models',0777)
+        os.mkdir('views',0777)
+        os.chdir('views')
+        os.mkdir('http_status',0777)
+        os.chdir('http_status')
+        htmlstring = '<html>\n'
+        htmlstring += '<head>\n'
+        htmlstring += '<title>404 Page Not Found</title>\n'
+        htmlstring += '</head>\n'
+        htmlstring += '<body>\n'
+        htmlstring += '<h1>Oops!</h1>\n'
+        htmlstring += '<h2>We could not find that page you were trying to reach.</h2>\n'
+        htmlstring += '<h3>Please check the URL and try again.</h3>\n'
+        htmlstring += '<h4>You can always go back to our <a href="/">home page</a>.</h4>\n'
+        htmlstring += '</body>\n'
+        htmlstring += '</html>\n'
+        f = open('404.html', 'w')
+        f.write(htmlstring)
+        f.close()
+        htmlstring = '<html>\n'
+        htmlstring += '<head>\n'
+        htmlstring += '<title>500 Internal Server Error</title>\n'
+        htmlstring += '</head>\n'
+        htmlstring += '<body>\n'
+        htmlstring += '<h1>Oops!</h1>\n'
+        htmlstring += '<h2>We are sorry but something went wrong.</h2>\n'
+        htmlstring += '<h3>We cannot serve any pages to you at this time.</h3>\n'
+        htmlstring += '<h4>Please try again later.</h4>\n'
+        htmlstring += '</body>\n'
+        htmlstring += '</html>\n'
+        f = open('500.html', 'w')
+        f.write(htmlstring)
+        f.close()
+        print 'An app named '+app_key+' has been created.'
+  elif base == 'bin':
+    for opt, arg in options:
+      if opt in ('--app'):
+        app_key = arg
+        os.mkdir(os.path.pardir+'/apps/'+app_key,0777)
+        os.chdir(os.path.pardir+'/apps/'+app_key)
+        os.mkdir('WEB-INF',0777)
+        os.chdir('WEB-INF')
+        xmlstring = '<?xml version="' + xml_version + '" encoding="' + encoding  + '" ?>\n'
+        xmlstring += '<web-app xmlns="http://java.sun.com/xml/ns/javaee" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://java.sun.com/xml/ns/javaee http://java.sun.com/xml/ns/javaee/web-app_2_5.xsd" metadata-complete="true" version="2.5">\n'
+        xmlstring += '<display-name>' + app_key + ' app</display-name>\n'
+        xmlstring += '<session-config>\n'
+        xmlstring += '<session-timeout>20</session-timeout>\n'
+        xmlstring += '</session-config>\n'
+        xmlstring += '<servlet>\n'
+        xmlstring += '<servlet-name>default</servlet-name>\n'
+        xmlstring += '<servlet-class>org.mortbay.jetty.servlet.DefaultServlet</servlet-class>\n'
+        xmlstring += '<init-param>\n'
+        xmlstring += '<param-name>dirAllowed</param-name>\n'
+        xmlstring += '<param-value>false</param-value>\n'
+        xmlstring += '</init-param>\n'
+        xmlstring += '<init-param>\n'
+        xmlstring += '<param-name>welcomeServlets</param-name>\n'
+        xmlstring += '<param-value>true</param-value>\n'
+        xmlstring += '</init-param>\n'
+        xmlstring += '</servlet>\n'
+        xmlstring += '<welcome-file-list>\n'
+        xmlstring += '<welcome-file>public/index.html</welcome-file>\n'
+        xmlstring += '</welcome-file-list>\n'
+        xmlstring += '<servlet>\n'
+        xmlstring += '<servlet-name>BaseServlet</servlet-name>\n'
+        xmlstring += '<servlet-class>src.main.servlet.BaseServlet</servlet-class>\n'
+        xmlstring += '</servlet>\n'
+        xmlstring += '<servlet-mapping>\n'
+        xmlstring += '<servlet-name>default</servlet-name>\n'
+        xmlstring += '<url-pattern>/</url-pattern>\n'
+        xmlstring += '</servlet-mapping>\n'
+        xmlstring += '<context-param>\n'
+        xmlstring += '<description>Unique App Key</description>\n'
+        xmlstring += '<param-name>app.key</param-name>\n'
+        xmlstring += '<param-value>' + app_key + '</param-value>\n'
+        xmlstring += '</context-param>\n'
+        xmlstring += '<error-page>\n'
+        xmlstring += '<error-code>404</error-code>\n'
+        xmlstring += '<location>/app/views/http_status/404.html</location>\n'
+        xmlstring += '</error-page>\n'
+        xmlstring += '<error-page>\n'
+        xmlstring += '<error-code>500</error-code>\n'
+        xmlstring += '<location>/app/views/http_status/500.html</location>\n'
+        xmlstring += '</error-page>\n'
+        xmlstring += '</web-app>\n'
+        f = open('web.xml', 'w')
+        f.write(xmlstring)
+        f.close()
+        os.chdir(os.path.pardir)
+        os.mkdir('app',0777)
+        os.mkdir('conf',0777)
+        os.chdir('conf')
+        xmlstring = '<?xml version="' + xml_version + '" encoding="' + encoding  + '" ?>\n'
+        xmlstring += '<routes>\n'
+        xmlstring += '<!--<url controller="'+ app_key  +'.app.controllers.Index">/index.jsp</url>-->\n'
+        xmlstring += '</routes>\n'
+        f = open('routes.xml', 'w')
+        f.write(xmlstring)
+        f.close()
+        os.chdir(os.path.pardir)
+        os.mkdir('public',0777)
+        os.chdir('public')
+        htmlstring = '<html>\n'
+        htmlstring += '<head>\n'
+        htmlstring += '<title>'+ app_key +'</title>\n'
+        htmlstring += '</head>\n'
+        htmlstring += '<body>\n'
+        htmlstring += '<h1>Welcome to '+ app_key +'.</h1>\n'
+        htmlstring += '<h2>This app is powered by Othello!</h2>\n'
+        htmlstring += '</body>\n'
+        htmlstring += '</html>\n'
+        f = open('index.html', 'w')
+        f.write(htmlstring)
+        f.close()
+        os.chdir(os.path.pardir)
+        os.chdir('app')
+        os.mkdir('controllers',0777)
+        os.mkdir('models',0777)
+        os.mkdir('views',0777)
+        os.chdir('views')
+        os.mkdir('http_status',0777)
+        os.chdir('http_status')
+        htmlstring = '<html>\n'
+        htmlstring += '<head>\n'
+        htmlstring += '<title>404 Page Not Found</title>\n'
+        htmlstring += '</head>\n'
+        htmlstring += '<body>\n'
+        htmlstring += '<h1>Oops!</h1>\n'
+        htmlstring += '<h2>We could not find that page you were trying to reach.</h2>\n'
+        htmlstring += '<h3>Please check the URL and try again.</h3>\n'
+        htmlstring += '<h4>You can always go back to our <a href="/">home page</a>.</h4>\n'
+        htmlstring += '</body>\n'
+        htmlstring += '</html>\n'
+        f = open('404.html', 'w')
+        f.write(htmlstring)
+        f.close()
+        htmlstring = '<html>\n'
+        htmlstring += '<head>\n'
+        htmlstring += '<title>500 Internal Server Error</title>\n'
+        htmlstring += '</head>\n'
+        htmlstring += '<body>\n'
+        htmlstring += '<h1>Oops!</h1>\n'
+        htmlstring += '<h2>We are sorry but something went wrong.</h2>\n'
+        htmlstring += '<h3>We cannot serve any pages to you at this time.</h3>\n'
+        htmlstring += '<h4>Please try again later.</h4>\n'
+        htmlstring += '</body>\n'
+        htmlstring += '</html>\n'
+        f = open('500.html', 'w')
+        f.write(htmlstring)
+        f.close()
+        print 'An app named '+app_key+' has been created.'
+  else:
+    sys.exit("Oops! This script must be run within the root/top level othello directory or othello/bin!")
+
+createapp()
